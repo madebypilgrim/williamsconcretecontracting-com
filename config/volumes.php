b4fa8219147c502a3af68986a5fe41078d255833
Trevor Plassman <tplassman@wrayward.com>
2019-02-25 21:20:59 -0500

    new build and project overview page.

--- config/volumes.php
+++ config/volumes.php
@@ -8,6 +8,10 @@ $assetsPath = getenv('HOME_PATH').getenv('WEB_PATH').getenv('ASSETS_PATH');
 $assetsUrl = getenv('ASSETS_URL').getenv('ASSETS_PATH');
 
 return [
+  'projectImages' => [
+    'path' => $assetsPath.'/images/project',
+    'url' => $assetsUrl.'/images/project',
+  ],
   'siteImages' => [
     'path' => $assetsPath.'/images/site',
     'url' => $assetsUrl.'/images/site',
